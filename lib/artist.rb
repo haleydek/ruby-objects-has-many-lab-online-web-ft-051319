0663dd2e7ecf80ba54f614bb60bf2e154357e899
Haley DeKeyser <hcd09@live.com>
2019-05-28 16:11:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,19 @@
 class Artist
   attr_accessor :name
-  @@songs = []
   
   def initialize(name)
     @name = name
+    @songs = []
   end
   
   def add_song(song)
-    @@songs << song.name
+    @songs << song
     song.artist = self
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
-    @@songs << song.name
+    @songs << song
     song.artist = self
   end
   
